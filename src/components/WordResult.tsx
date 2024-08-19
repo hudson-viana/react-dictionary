8fce2ec242a0206142e73355eb11da895f1189dd
Hudson Santos <work.of.hudson@gmail.com>
2024-08-19 10:20:00 -0300

    refactor: this component required refactoring after adding the H1 tag in app.tsx

--- src/components/WordResult.tsx
+++ src/components/WordResult.tsx
@@ -1,27 +1,27 @@
 import { Result } from "../App";
 
 type WordResultProps = {
-  data: Result[],
-}
+  data: Result[];
+};
 
-export default function WordResult({data}:WordResultProps) {
+export default function WordResult({ data }: WordResultProps) {
   return (
     <>
       {data.length > 0 && (
-        <article className="mx-40 max-w-[70%] mt-14">
+        <article className="mx-40 mt-14 max-w-[70%]">
           <ul>
             {data.map((d, dataIndex) => (
               // each word do not have a unique id, so index is used
               <li key={dataIndex} className="mb-14">
-                <h1 className="text-5xl font-extrabold mb-5 text-white">
+                <h2 className="text-5xl font-extrabold mb-5 text-green-500">
                   {d.word}
-                </h1>
+                </h2>
                 <ul>
                   {d.meanings.map((meaning, meaningsIndex) => (
                     <li key={meaningsIndex} className="mb-10">
-                      <h2 className="text-2xl italic mb-4 text-white">
+                      <h3 className="text-2xl italic mb-4 text-white font-bold">
                         {meaning.partOfSpeech}
-                      </h2>
+                      </h3>
                       <ol className="list-decimal list-inside ml-5 mb-8">
                         {meaning.definitions.map((def, meaningIndex) => (
                           <li key={meaningIndex} className="mb-3">
@@ -31,7 +31,7 @@ export default function WordResult({data}:WordResultProps) {
                       </ol>
                       {meaning.synonyms?.length > 0 && (
                         <section className="flex mb-5">
-                          <h3 className="font-bold ml-5">Synonyms: </h3>
+                          <h4 className="font-bold ml-5">Synonyms: </h4>
                           <ul className="ml-3 flex flex-wrap">
                             {meaning.synonyms.map((synonym, synonymsIndex) => (
                               <li
@@ -47,7 +47,7 @@ export default function WordResult({data}:WordResultProps) {
 
                       {meaning.antonyms?.length > 0 && (
                         <section className="flex mb-5">
-                          <h3 className="font-bold ml-5">Antonyms: </h3>
+                          <h4 className="font-bold ml-5">Antonyms: </h4>
                           <ul className="ml-3 flex flex-wrap">
                             {meaning.antonyms.map((antonym, antonymsIndex) => (
                               <li
@@ -64,21 +64,21 @@ export default function WordResult({data}:WordResultProps) {
                   ))}
                 </ul>
                 <aside className="mt-10">
+                  <a href={d.license.url} className="text-sm opacity-45">
+                    License: {d.license.name}
+                  </a>
                   {d.sourceUrls?.length > 0 && (
-                    <div className="text-sm opacity-45 flex gap-2">
-                      <p>Sources of the definitions: </p>
-                      <ul className="flex gap-3 list-decimal list-inside">
+                    <div className="text-sm opacity-45 flex gap-2 mt-1">
+                      <p className="min-w-[8rem]">Definitions from: </p>
+                      <ul className="flex list-decimal list-inside flex-wrap">
                         {d.sourceUrls.map((sourceUrl, sourceUrlsIndex) => (
-                          <li key={sourceUrlsIndex}>
+                          <li key={sourceUrlsIndex} className="mr-4 mb-1">
                             <a href={sourceUrl}> {sourceUrl}</a>
                           </li>
                         ))}
                       </ul>
                     </div>
                   )}
-                  <a href={d.license.url} className="text-sm opacity-45">
-                    License: {d.license.name}
-                  </a>
                 </aside>
               </li>
             ))}
