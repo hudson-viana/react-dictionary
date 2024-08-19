42c82df9144b8eabb1ced986153d9ba391db3299
Hudson Santos <work.of.hudson@gmail.com>
2024-08-19 07:48:18 -0300

    style: set font family and apply dark background to cover the entire screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
-import './index.css'
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -16,8 +13,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="min-h-screen fontFamily bg-[#232529] text-[#b3b5ba]  pt-14 flex flex-col items-center">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
